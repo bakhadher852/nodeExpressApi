32b0bfc8bcab9559ebb944d7aafa51e643459589
bakhadher <mohammed85270@gmail.com>
2023-08-23 21:03:58 +0800

    OneToOneSessions model

---
+++ models/OneToOneSessions.js
@@ -0,0 +1,38 @@
+// models/oneToOneSessions.js
+const DataTypes = require("sequelize");
+const db = require("../config/database");
+
+const oneToOneSessions = db.define("oneToOneSessions", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  timestamp: {
+    type: DataTypes.timestamp,
+    allowNull: false,
+  },
+  duration: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  subject: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sescription: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+//If the table not exict it will creat it in DB
+// oneToOneSessions
+//   .sync()
+//   .then((result) => {
+//     console.log("oneToOneSessions talbel added successfully", result);
+//   })
+//   .catch((err) => {
+//     console.log("oneToOneSessions talbel NOT added ", err);
+//   });
+oneToOneSessions.sync();
+module.exports = oneToOneSessions;
