08c26a23b3dc58bd6da5db2e9807cb5702afa705
bakhadher <mohammed85270@gmail.com>
2023-08-23 21:10:50 +0800

    OneToOneSessions

---
+++ routes/OneToOneSessions.js
@@ -0,0 +1,14 @@
+// routes/oneToOneSessions.js
+const express = require("express");
+const router = express.Router();
+const oneToOneSessionsMod = require("../models/oneToOneSessions");
+const oneToOneSessionsController = require("../controllers/oneToOneSessions");
+// res.send("Router")
+
+router.get("/", oneToOneSessionsController.list);
+router.get("/:id", oneToOneSessionsController.getById);
+router.post("/add", oneToOneSessionsController.create);
+router.delete("/delete/:id", oneToOneSessionsController.delete);
+router.put("/update/:id", oneToOneSessionsController.update);
+
+module.exports = router;
