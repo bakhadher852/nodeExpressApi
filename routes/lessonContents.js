93d07a01e8f65c63bc9bee8e54b3ea0dec8d6e35
bakhadher <mohammed85270@gmail.com>
2023-09-23 12:44:30 +0800

    add permissions to LessonContents

--- routes/lessonContents.js
+++ routes/lessonContents.js
@@ -11,10 +11,30 @@ id:      EndpointName:
 5        Delete
 */
 
-router.get("/", lessonContentsController.list);
-router.get("/:id", lessonContentsController.getById);
-router.post("/add", lessonContentsController.create);
-router.put("/update/:id", lessonContentsController.update);
-router.delete("/delete/:id", lessonContentsController.delete);
+router.get(
+  "/",
+  checkPermission("LessonContents", 1),
+  lessonContentsController.list
+);
+router.get(
+  "/:id",
+  checkPermission("LessonContents", 2),
+  lessonContentsController.getById
+);
+router.post(
+  "/",
+  checkPermission("LessonContents", 3),
+  lessonContentsController.create
+);
+router.put(
+  "/:id",
+  checkPermission("LessonContents", 4),
+  lessonContentsController.update
+);
+router.delete(
+  "/:id",
+  checkPermission("LessonContents", 5),
+  lessonContentsController.delete
+);
 
 module.exports = router;
