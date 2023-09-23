51e610b6bb5805cdcffd2cea2381b7f8d3ef371d
bakhadher <mohammed85270@gmail.com>
2023-09-23 12:35:24 +0800

    add permissons to units

--- routes/units.js
+++ routes/units.js
@@ -1,14 +1,19 @@
 // routes/units.js
 const express = require("express");
 const router = express.Router();
-const unitsMod = require("../models/units");
 const unitsController = require("../controllers/units");
-// res.send("Router")
+/*
+id:      EndpointName:
+1        getAll
+2        Get
+3        Create
+4        Update
+5        Delete
+*/
 
-router.get("/", unitsController.list);
-router.get("/:id", unitsController.getById);
-router.post("/add", unitsController.create);
-router.delete("/delete/:id", unitsController.delete);
-router.put("/update/:id", unitsController.update);
-
+router.get("/", checkPermission("Units", 1), unitsController.list);
+router.get("/:id", checkPermission("Units", 2), unitsController.getById);
+router.post("/", checkPermission("Units", 3), unitsController.create);
+router.put("/:id", checkPermission("Units", 4), unitsController.update);
+router.delete("/:id", checkPermission("Units", 5), unitsController.delete);
 module.exports = router;
